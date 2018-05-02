7a59e6f645e7374ac66b461acfbb5e77ae30ca03
Kairo Costa <kairocosta98@gmail.com>
2018-05-02 16:50:49 -0300

    Update BooksGoogle.py

--- BooksGoogle.py
+++ BooksGoogle.py
@@ -1,8 +1,7 @@
 import requests
 
 titulo = input("Digite o livro que você deseja encontrar...")
-request = requests.get("https://www.googleapis.com/books/v1/volumes?q=python&key={YOUR_API_KEY}
-").json()
+request = requests.get("https://www.googleapis.com/books/v1/volumes?q=python&key={YOUR_API_KEY}").json()
 
 for i in request["items"]:
     print(i["volumeInfo"]["title"])
